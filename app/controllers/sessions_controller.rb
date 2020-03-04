5ac22a4fbfffd16d879f3f0f9120cb1c5b74a423
Moutasem Manaseer <mo3manaseer@gmail.com>
2020-03-04 13:35:50 +0200

    Added routes and basic pages for each of the routes.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+  def create
+
+  end
+  def destroy
+
+  end
+
+  private
+  def user_params
+
+  end
+end
---
+++ app/controllers/submissions_controller.rb
@@ -0,0 +1,22 @@
+class SubmissionsController < ApplicationController
+  def index
+
+  end
+  def show
+
+  end
+  def new
+
+  end
+  def create
+
+  end
+  def destroy
+
+  end
+
+  private
+  def submission_params
+
+  end
+end
