515acc6fdbf7f07ba5ad3a273cc85665d534d51f
Moutasem Manaseer <mo3manaseer@gmail.com>
2020-04-21 22:31:39 +0300

    fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,9 @@ class ApiController < ApplicationController
   include ExecuteHelper
   skip_before_action :verify_authenticity_token
   def submit
-      code = Code.find(params[:id])
+    logger.error { params}
+    logger.error { Parameters}
+      code = Code.find(Parameters[:id])
       submission = execute(code.info,params[:input])
       @testsub = Testsub.new(code_id: code.id, input: params[:input], output: submission[:output])
       @testsub.save
